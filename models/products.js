a554097100af5db5b00d0bad393c8d45fe81f8e6
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2021-10-09 19:36:31 -0400

    re routed everything and fixed all functionality

--- models/products.js
+++ models/products.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-	name: { type: String },
-	description: { type: String },
-	img: { type: String },
-    price: { type: Number },
-    qty: { type: Number },
+const storeSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    img: String,
+    price: Number,
+    qty: Number
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Store = mongoose.model('Store', storeSchema);
 
-module.exports = Product;
+module.exports = Store;
